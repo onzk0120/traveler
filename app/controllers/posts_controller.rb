3769ecacd6f97c5c65309effcd159a6cfdb29f58
onzk0120 <222937425+onzk0120@users.noreply.github.com>
2025-08-23 23:33:15 +0900

    ユーザ詳細ページuser_idを投稿時の情報に追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:location, :text, :image)
+    params.require(:post).permit(:user_id, :location, :text, :image)
   end
 
 end
