794ae3ac48d32c2b68f17a91976d81d04a4e3ce9
onzk0120 <222937425+onzk0120@users.noreply.github.com>
2025-08-24 22:05:11 +0900

    ユーザ詳細ページ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new(post_params)
+    post.user_id = current_user.id
     post.save
     redirect_to post_path(post.id)
   end
